bedb1d6fd21aa68e8547d2dcabfed478fcf7dcc0
Guilherme Paixão <guiferpa@gmail.com>
2023-04-10 23:06:14 -0300

    chore: added .editconfig file

--- src/index.ts
+++ src/index.ts
@@ -4,32 +4,32 @@ import { Lexer } from "./lexer";
 import { Parser } from "./parser";
 
 async function read(path: string): Promise<Buffer> {
-	return new Promise((resolve, reject) => {
-		let buffer = Buffer.from("");
+  return new Promise((resolve, reject) => {
+    let buffer = Buffer.from("");
 
-		const reader = fs.createReadStream(path);
+    const reader = fs.createReadStream(path);
 
-		reader.on('data', (chunk: Buffer) => {
-			buffer = Buffer.concat([buffer, chunk]);
-		});
+    reader.on('data', (chunk: Buffer) => {
+      buffer = Buffer.concat([buffer, chunk]);
+    });
 
-		reader.on('error', (err) => {
-			reject(err);
-		});
+    reader.on('error', (err) => {
+      reject(err);
+    });
 
-		reader.on('close', () => {
-			resolve(buffer);
-		});
-	});
+    reader.on('close', () => {
+      resolve(buffer);
+    });
+  });
 }
 
 
 ;(async () => {
-	const buffer = await read('./test.ar');
+  const buffer = await read('./test.ar');
 
-	const lexer = new Lexer(buffer); // Tokenizer
-	const p = new Parser(lexer);
-	const ast = p.parse();
+  const lexer = new Lexer(buffer); // Tokenizer
+  const p = new Parser(lexer);
+  const ast = p.parse();
 
-	console.log(JSON.stringify(ast, null, 2));
+  console.log(JSON.stringify(ast, null, 2));
 })();
--- src/lexer.ts
+++ src/lexer.ts
@@ -1,83 +1,83 @@
 import {Token, TokenIdentifier, TokenRecordList, TokenSpecList} from "./tokens";
 
 export class Lexer {
-	public _cursor = 0;
-	public readonly _buffer: Buffer;
+  public _cursor = 0;
+  public readonly _buffer: Buffer;
 
-	constructor(buffer: any) {
-		this._buffer = buffer;
-	}
+  constructor(buffer: any) {
+    this._buffer = buffer;
+  }
 
-	public async tokenify(): Promise<any> {
-		const tokens: any[] = [];
+  public async tokenify(): Promise<any> {
+    const tokens: any[] = [];
 
-		while (this.hasMoreTokens()) {
-			const token = await this.getAsyncNextToken();
-			tokens.push(token);
-		}
+    while (this.hasMoreTokens()) {
+      const token = await this.getAsyncNextToken();
+      tokens.push(token);
+    }
 
-		// Info about end of token list
-		tokens.push(null);
+    // Info about end of token list
+    tokens.push(null);
 
-		this._cursor = 0;
+    this._cursor = 0;
 
-		return tokens;
-	}
+    return tokens;
+  }
 
-	public hasMoreTokens(): boolean {
-		return this._cursor < this._buffer.length;
-	}
+  public hasMoreTokens(): boolean {
+    return this._cursor < this._buffer.length;
+  }
 
-	public getNextToken(): Token {
-		if (!this.hasMoreTokens()) return {
-			id: TokenIdentifier.EOT,
-			type: TokenRecordList[TokenIdentifier.EOT],
-			value: ""
-		}
+  public getNextToken(): Token {
+    if (!this.hasMoreTokens()) return {
+      id: TokenIdentifier.EOT,
+      type: TokenRecordList[TokenIdentifier.EOT],
+      value: ""
+    }
 
-		const str = this._buffer.toString('utf-8', this._cursor);
+    const str = this._buffer.toString('utf-8', this._cursor);
 
-		for (const [regex, tokenType, tokenId] of TokenSpecList) {
-			const tokenValue = this._match(regex, str);
+    for (const [regex, tokenType, tokenId] of TokenSpecList) {
+      const tokenValue = this._match(regex, str);
 
-			if (tokenValue === null) continue;
+      if (tokenValue === null) continue;
 
-			if (tokenId === TokenIdentifier.WHITESPACE)
-				return this.getNextToken();
+      if (tokenId === TokenIdentifier.WHITESPACE)
+        return this.getNextToken();
 
-			return {id: tokenId, type: tokenType, value: tokenValue}
-		}
+      return {id: tokenId, type: tokenType, value: tokenValue}
+    }
 
-		throw new SyntaxError(`Unexpected token: ${str}`);
-	}
+    throw new SyntaxError(`Unexpected token: ${str}`);
+  }
 
-	public async getAsyncNextToken(): Promise<Token | null> {
-		if (!this.hasMoreTokens()) return null;
+  public async getAsyncNextToken(): Promise<Token | null> {
+    if (!this.hasMoreTokens()) return null;
 
-		const str = this._buffer.toString('utf-8', this._cursor);
+    const str = this._buffer.toString('utf-8', this._cursor);
 
-		for (const [regex, tokenType, tokenId] of TokenSpecList) {
-			const tokenValue = this._match(regex as RegExp, str);
+    for (const [regex, tokenType, tokenId] of TokenSpecList) {
+      const tokenValue = this._match(regex as RegExp, str);
 
-			if (tokenValue === null) continue;
+      if (tokenValue === null) continue;
 
-			if (tokenId === TokenIdentifier.WHITESPACE) {
-				return this.getAsyncNextToken();
-			}
+      if (tokenId === TokenIdentifier.WHITESPACE) {
+        return this.getAsyncNextToken();
+      }
 
-			return { id: tokenId, type: tokenType, value: tokenValue }
-		}
+      return { id: tokenId, type: tokenType, value: tokenValue }
+    }
 
-		throw new SyntaxError(`Unexpected token: ${str}`);
-	}
+    throw new SyntaxError(`Unexpected token: ${str}`);
+  }
 
-	private _match(product: RegExp, str: string) {
-		const matched = product.exec(str);
-		if (matched === null) return null
+  private _match(product: RegExp, str: string) {
+    const matched = product.exec(str);
+    if (matched === null) return null
 
-		const value = matched[0];
-		this._cursor += value.length;
-		return value;
-	}
+    const value = matched[0];
+    this._cursor += value.length;
+    return value;
+  }
 }
 

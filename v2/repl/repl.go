b25aad2f0580a447929d7b080a71e7ee47f4d1e4
Guilherme Paixão <guiferpa@gmail.com>
2024-08-09 14:56:40 -0300

    feat: created package for repl

---
+++ v2/repl/repl.go
@@ -0,0 +1,31 @@
+package repl
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/guiferpa/aurora/lexer"
+)
+
+const PROMPT = ">> "
+
+func Start(in io.Reader, out io.Writer) {
+	scanner := bufio.NewScanner(in)
+	for {
+		fmt.Fprintf(out, PROMPT)
+		scanned := scanner.Scan()
+		if !scanned {
+			return
+		}
+
+		line := bytes.NewBufferString(scanner.Text())
+		l, err := lexer.GetTokensGivenBytes(line.Bytes())
+		if err != nil {
+			fmt.Println(err)
+			continue
+		}
+		fmt.Println(l)
+	}
+}

a10fa80df70bd21f800eb7aeed54d69711ef95ab
Guilherme Paixão <guiferpa@gmail.com>
2024-08-10 14:24:28 -0300

    refact: changed print package

--- v2/print/json.go
+++ v2/print/json.go
@@ -3,15 +3,7 @@ package print
 import (
 	"encoding/json"
 	"io"
-
-	"github.com/TylerBrock/colorjson"
 )
-
-var f = colorjson.NewFormatter()
-
-func init() {
-	f.Indent = 2
-}
 
 func JSON(w io.Writer, o interface{}) error {
 	enc := json.NewEncoder(w)

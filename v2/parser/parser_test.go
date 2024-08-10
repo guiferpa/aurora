396ecec95c3b1463be16b5ef101f0fdf52fb006a
Guilherme Paixão <guiferpa@gmail.com>
2024-08-09 22:14:07 -0300

    chore: created some cases for parser package

---
+++ v2/parser/parser_test.go
@@ -0,0 +1,76 @@
+package parser
+
+import (
+	"testing"
+
+	"github.com/guiferpa/aurora/lexer"
+)
+
+type tok struct {
+	tag lexer.Tag
+}
+
+func (t tok) GetMatch() []byte {
+	return []byte{}
+}
+
+func (t tok) GetTag() lexer.Tag {
+	return t.tag
+}
+
+func (t tok) GetLine() int {
+	return 0
+}
+
+func (t tok) GetColumn() int {
+	return 0
+}
+
+func (t tok) GetCursor() int {
+	return 0
+}
+
+func TestEatTokenWithEmptySlice(t *testing.T) {
+	tokens := []lexer.Token{}
+	p := &pr{cursor: 0, tokens: tokens}
+	got, err := p.EatToken(lexer.BREAK_LINE)
+	if err != nil {
+		t.Error("unexpected error when eat some token:", err)
+	}
+	if got != nil {
+		t.Errorf("unexpected token when try eat empty slice, got: %v", got)
+	}
+}
+
+func TestEatToken(t *testing.T) {
+	cases := []struct {
+		Tokens   []lexer.Token
+		TokenIds []string
+	}{
+		{
+			[]lexer.Token{
+				tok{lexer.TagAssign},
+				tok{lexer.TagAssign},
+				tok{lexer.TagSum},
+			},
+			[]string{
+				lexer.ASSIGN,
+				lexer.ASSIGN,
+				lexer.SUM,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		p := &pr{cursor: 0, tokens: c.Tokens}
+		for _, tid := range c.TokenIds {
+			got, err := p.EatToken(tid)
+			if err != nil {
+				t.Error("unexpected error when eat some token:", err)
+			}
+			if got.GetTag().Id != tid {
+				t.Errorf("unexpected token when eat, got: %v, expected: %s", got.GetTag().Id, tid)
+			}
+		}
+	}
+}

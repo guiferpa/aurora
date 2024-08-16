18bb8c33830e202cf2898ce5b3dd98be1fa9bb44
Guilherme Paixão <guiferpa@gmail.com>
2024-08-16 10:16:16 -0300

    fix: changed fill funcion for minimun byte size

--- v2/emitter/emitter.go
+++ v2/emitter/emitter.go
@@ -33,6 +33,9 @@ func (e *emt) genLabel() []byte {
 
 func (e *emt) fill64Bits(bfs []byte) []byte {
 	const size = 8
+	if len(bfs) >= size {
+		return bfs
+	}
 	bs := make([]byte, size)
 	for i := 0; i < len(bfs); i++ {
 		bs[(size-len(bfs))+i] = bfs[i]

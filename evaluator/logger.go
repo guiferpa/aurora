b08ebb12164293ca669485ffb00714e7e792d137
Guilherme Paixão <guiferpa@gmail.com>
2025-12-30 10:33:29 -0300

    chore: improved logger for evaluator

--- evaluator/logger.go
+++ evaluator/logger.go
@@ -2,20 +2,48 @@ package evaluator
 
 import (
 	"fmt"
+	"os"
+	"text/tabwriter"
 
+	"github.com/fatih/color"
+	"github.com/guiferpa/aurora/byteutil"
 	"github.com/guiferpa/aurora/emitter"
 )
 
+var yellow = color.New(color.FgHiYellow).SprintFunc()
+var magenta = color.New(color.FgHiMagenta).SprintFunc()
+
 type Logger struct {
 	enableLogging bool
+	w             *tabwriter.Writer
 }
 
 func NewLogger(enableLogging bool) *Logger {
-	return &Logger{enableLogging: enableLogging}
+	return &Logger{
+		enableLogging: enableLogging,
+		w:             tabwriter.NewWriter(os.Stdout, 0, 0, 2, ' ', 0),
+	}
 }
 
-func (l *Logger) Println(opcode byte) {
+func (l *Logger) Println(label []byte, opcode byte, left, right []byte) error {
 	if l.enableLogging {
-		fmt.Println(emitter.ResolveOpCode(opcode))
+		resolveOpcode := color.New(color.FgHiCyan).Sprint(emitter.ResolveOpCode(opcode))
+		colorizedLeft := magenta("<empty>")
+		if len(left) > 0 {
+			colorizedLeft = yellow(byteutil.ToHexBloom(left))
+		}
+		colorizedRight := magenta("<empty>")
+		if len(right) > 0 {
+			colorizedRight = yellow(byteutil.ToHexBloom(right))
+		}
+		labelHex := yellow(byteutil.ToHexBloom(label))
+		if _, err := fmt.Fprintf(l.w, "%s\t%s\t%s\t%s\n", labelHex, resolveOpcode, colorizedLeft, colorizedRight); err != nil {
+			return err
+		}
 	}
+	return nil
+}
+
+func (l *Logger) Close() error {
+	return l.w.Flush()
 }

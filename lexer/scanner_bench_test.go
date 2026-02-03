e0edf28d6d2d2d313cdd99982db65ac6bb9a1a08
Guilherme Paixão <guiferpa@gmail.com>
2026-02-03 13:20:17 -0300

    chore: removed unnecessary benchs with old source code
    Read more details related these bench here: https://github.com/guiferpa/aurora/pull/7

--- lexer/scanner_bench_test.go
+++ lexer/scanner_bench_test.go
@@ -4,33 +4,6 @@ import (
 	"testing"
 )
 
-var (
-	// Simple expression
-	simpleInput = []byte(`ident foo = 123;`)
-
-	// Medium complexity - if statement
-	mediumInput = []byte(`ident result = if 10 bigger 5 { 100; } else { 200; };`)
-
-	// Complex - multiple statements with function calls
-	complexInput = []byte(`
-ident fib = {
-  ident n = arguments 0;
-  if n smaller 1 or n equals 1 { n; } else { fib(n - 1) + fib(n - 2); };
-};
-ident result = fib(10);
-print result;
-`)
-
-	// Hexadecimal numbers
-	hexInput = []byte(`ident hex_ff = 0xFF; ident hex_10 = 0x10; ident sum = hex_ff + hex_10;`)
-
-	// String heavy
-	stringInput = []byte(`ident greeting = "hello"; ident name = "world"; print greeting; print name;`)
-
-	// Many tokens
-	manyTokensInput = []byte(`ident a = 1; ident b = 2; ident c = 3; ident d = 4; ident e = 5; ident f = 6; ident g = 7; ident h = 8; ident i = 9; ident j = 10;`)
-)
-
 func BenchmarkScanToken_Keyword(b *testing.B) {
 	input := []byte(`ident`)
 	for i := 0; i < b.N; i++ {
@@ -80,47 +53,6 @@ func BenchmarkScanToken_Comment(b *testing.B) {
 	}
 }
 
-func BenchmarkGetTokens_Simple(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetTokens(simpleInput)
-	}
-}
-
-func BenchmarkGetTokens_Medium(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetTokens(mediumInput)
-	}
-}
-
-func BenchmarkGetTokens_Complex(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetTokens(complexInput)
-	}
-}
-
-func BenchmarkGetTokens_Hex(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetTokens(hexInput)
-	}
-}
-
-func BenchmarkGetTokens_Strings(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetTokens(stringInput)
-	}
-}
-
-func BenchmarkGetTokens_ManyTokens(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetTokens(manyTokensInput)
-	}
-}
-
-func BenchmarkGetFilledTokens_Complex(b *testing.B) {
-	for i := 0; i < b.N; i++ {
-		_, _ = GetFilledTokens(complexInput)
-	}
-}
 func BenchmarkIsIdentChar(b *testing.B) {
 	chars := []byte("abcABC123_")
 	for i := 0; i < b.N; i++ {
